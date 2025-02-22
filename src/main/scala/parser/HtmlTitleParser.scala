f9698a306b85e3faa469b69bcc6fcc2da7ce660d
senyanyash <senyanyash64@gmail.com>
2025-02-22 13:01:11 +0300

    fix handling redirections

--- src/main/scala/parser/HtmlTitleParser.scala
+++ src/main/scala/parser/HtmlTitleParser.scala
@@ -3,16 +3,18 @@ package parser
 import config.Configs.CrawlerConfig
 import model.CrawlerProtocol.CrawlerResponse
 import org.jsoup.Jsoup
-import zio.{URIO, ZIO, durationInt}
-import zio.http.{Charsets, Client, Request, URL}
+import zio.{URIO, ZIO}
+import zio.http.{Charsets, Client, Request, URL, ZClientAspect}
 
 object HtmlTitleParser {
 
   def parseTitleForUrl(url: URL): URIO[Client with CrawlerConfig, CrawlerResponse] = for {
     cfg <- ZIO.service[CrawlerConfig]
-    res <- Client.batched(Request.get(url))
+    client <- ZIO.service[Client].map(_ @@ ZClientAspect.followRedirects(5)((_, _) => ZIO.fail("Ошибка редиректа")))
+    res <- client.batched(Request.get(url))
+      .filterOrFail(_.status.isSuccess)("Ошибка при запросе к сайту")
       .flatMap(_.body.asString(Charsets.Utf8))
-      .mapError(_ => "Ошибка при запросе к сайту")
+      .mapError(_ => "Ошибка: не удалось достать тело запроса")
       .timeout(cfg.timeout)
       .someOrFail("Ошибка: не удалось уложиться в таймаут")
       .flatMap(Jsoup.parse(_).title match {
